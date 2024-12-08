4cf15eb6a1acdb276d56b83c92f3153e1496c3b2
yusril.mahendri <yusrilmahendri.yusril@gmail.com>
2024-12-08 15:14:25 +0700

    repair id riwayat & ucapan

--- app/Http/Resources/Bukutamu/PengunjungResource.php
+++ app/Http/Resources/Bukutamu/PengunjungResource.php
@@ -17,6 +17,7 @@ class PengunjungResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id_riwayat' => $this->id,
             'user' => new UserResource($this->user),
             'nama' => $this->nama,
             'tanggal' => Carbon::parse($this->created_at)->format('Y-m-d H:i:s'),
