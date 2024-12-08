19676b8001d591ca1f249763fd3a380408674d25
yusril.mahendri <yusrilmahendri.yusril@gmail.com>
2024-12-09 00:17:15 +0700

    paginate, limit and search bukutamu

--- app/Http/Resources/Bukutamu/BukuTamuCollection.php
+++ app/Http/Resources/Bukutamu/BukuTamuCollection.php
@@ -16,16 +16,21 @@ class BukuTamuCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        // all count tamu
-        $allCountTamu = $this->collection->count();
-
-        // Total tamu hari ini
+        $allCountTamu = $this->resource->total(); // Total guests across all pages
         $todayCountTamu = $this->collection->filter(function ($item) {
             return Carbon::parse($item->created_at)->isToday();
         })->count();
 
         return [
             'data' => BukuTamuResource::collection($this->collection),
+            'pagination' => [
+                'total' => $this->resource->total(),
+                'per_page' => $this->resource->perPage(),
+                'current_page' => $this->resource->currentPage(),
+                'last_page' => $this->resource->lastPage(),
+                'from' => $this->resource->firstItem(),
+                'to' => $this->resource->lastItem(),
+            ],
             'all_count_tamu' => $allCountTamu,
             'today_count_tamu' => $todayCountTamu,
         ];
