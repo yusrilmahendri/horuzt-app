911f550543fcfcc78105ee72cf17b1b8fe3751a2
yusril.mahendri <yusrilmahendri.yusril@gmail.com>
2025-01-29 00:55:42 +0700

    display roles

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,29 +9,34 @@ use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
         // Sign in user
-        public function login(Request $request)
-        {
-            $request->validate([
-                'email' => 'required|string|email',
-                'password' => 'required|string',
-            ]);
-    
-            $credentials = $request->only('email', 'password');
-    
-            if (!Auth::attempt($credentials)) {
-                return response()->json(['message' => 'Invalid login details'], 401);
-            }
-    
-            $user = Auth::user();
-    
-            // Generate token
-            $token = $user->createToken('auth_token')->plainTextToken;
-    
-            return response()->json([
-                'access_token' => $token,
-                'token_type' => 'Bearer',
-            ]);
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid login details'], 401);
         }
+
+        $user = Auth::user();
+
+        // Generate token
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        // Retrieve roles
+        $roles = $user->getRoleNames(); // Returns a collection of role names assigned to the user
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'roles' => $roles,
+        ]);
+    }
+
     
         // Log out user
         public function logout(Request $request)
