7e21d64cacfddc4a2dbd6c0f8d1152796a77264d
rafi.hariman <231232609@uii.ac.id>
2025-05-28 10:34:52 +0700

    fix: comment out the run method in UserTableSeeder for clarity

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -11,17 +11,17 @@ class UserTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run()
-    {
-        $user = User::create([
-            'name' => 'hanif',
-            'email' => 'hanif@gmail.com',
-            'email_verified_at' => now(),
-            'password' => bcrypt('12345678'),
-            'email_verified_at' => now(),
-        ]);
-        $user->assignRole('user');
-        $user->createToken('auth_token')->plainTextToken;
-        return $user;
-    }
+    // public function run()
+    // {
+    //     $user = User::create([
+    //         'name' => 'hanif',
+    //         'email' => 'hanif@gmail.com',
+    //         'email_verified_at' => now(),
+    //         'password' => bcrypt('12345678'),
+    //         'email_verified_at' => now(),
+    //     ]);
+    //     $user->assignRole('user');
+    //     $user->createToken('auth_token')->plainTextToken;
+    //     return $user;
+    // }
 }
